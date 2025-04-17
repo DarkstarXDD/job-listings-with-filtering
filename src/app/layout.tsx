54acbf2ac73a3740de9818968c0c4cd0f993c621
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-04-17 08:53:32 +0530

    chore: initialize project and add meta tags

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next"
+import { League_Spartan } from "next/font/google"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "Job Listings with Filtering",
+  description:
+    "Check out Darkstar's solution for the Job Listings with Filtering challenge on Frontend Mentor",
+
+  authors: {
+    name: "Darkstar",
+    url: "https://github.com/DarkstarXDD",
+  },
+
+  openGraph: {
+    type: "website",
+    url: "https://job-listings-with-filtering-darkstarxdd.vercel.app/",
+    title: "devfinder",
+
+    description:
+      "Check out Darkstar's solution for the Job Listings with Filtering challenge on Frontend Mentor",
+
+    images: {
+      url: "https://job-listings-with-filtering-darkstarxdd.vercel.app/og-img.jpeg",
+      type: "image/jpeg",
+      width: 1440,
+      height: 756,
+    },
+  },
+}
+
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--font-league-spartan",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${leagueSpartan.variable} font-league-spartan bg-zinc-800 text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
