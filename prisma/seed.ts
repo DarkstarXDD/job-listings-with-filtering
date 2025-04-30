17a359b97a3679050f1afc890c295bb829cac218
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-04-30 08:30:54 +0530

    refactor: update prisma schema and seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,10 +4,18 @@ import { jobs } from "../public/jobsData"
 const prisma = new PrismaClient()
 
 async function main() {
+  await prisma.job.deleteMany()
+  await prisma.company.deleteMany()
+  await prisma.role.deleteMany()
+  await prisma.language.deleteMany()
+  await prisma.tool.deleteMany()
+
   for (const job of jobs) {
     await prisma.job.create({
       data: {
         position: job.position,
+        isNew: job.new,
+        isFeatured: job.featured,
         level: job.level as Level,
         location: job.location,
         contract: job.contract as Contract,
